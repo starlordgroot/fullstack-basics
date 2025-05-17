a44fdc18e2b382bca79a88ae81d760bafe006c52
David Wise <dwise5@students.towson.edu>
2025-05-17 17:07:01 -0400

    Part 1

---
+++ src/main/java/com/example/fullstack/basics/model/Person.java
@@ -0,0 +1,35 @@
+package com.example.fullstack.basics.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}
