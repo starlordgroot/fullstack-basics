b40e52203eca1706878bece9da1e2a99f6a95665
David Wise <dwise5@students.towson.edu>
2025-05-19 01:44:22 -0400

    Added getters/setters for ID value, as it was not being displayed.

--- src/main/java/com/example/fullstack/basics/model/Person.java
+++ src/main/java/com/example/fullstack/basics/model/Person.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Person {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -17,6 +17,14 @@ public class Person {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setFirstName(String firstName) {
