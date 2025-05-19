050d190ad183d0a035dc5f7c9f28e8d28f2652ca
David Wise <dwise5@students.towson.edu>
2025-05-18 22:42:22 -0400

    Adding Person Repository class. Example of Custom @Query using JPQL.

--- src/main/java/com/example/fullstack/basics/repository/PersonRepository.java
+++ src/main/java/com/example/fullstack/basics/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package com.example.fullstack.basics.repository;
 
 
 import com.example.fullstack.basics.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
     Person findByFirstName(String firstName);
+
+    //Example of @Query annotation in JPQL
+    @Query("SELECT p FROM Person p WHERE p. firstName LIKE %:name%")
+    Person findByThierName(String name);
 
 }
